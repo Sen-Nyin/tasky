d905679ea5eeb189c1e6cac62d7dbe93290f18a1
Leon Lonsdale <leonlonsdale@icloud.com>
2022-09-22 22:23:38 +0100

    built modal, overlay, new task form. Added logic for open/close modal on click for cancel button (modal) and add task button (header)

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ import View from './view';
 
 class Controller {
   constructor(model, view) {}
+
   handleAddTask() {}
   handleEditTask() {}
   handleDeleteTask() {}
--- src/view.js
+++ src/view.js
@@ -3,7 +3,21 @@
 // ##########################################################
 
 export default class View {
-  constructor() {}
+  constructor() {
+    this.elementNavContainer = this.findEle('[data-label="navcontainer"]');
+    this.elementNavbar = this.findEle('[data-label="navbar"]');
+    this.buttonAddTaskMain = this.findEle('[data-label="addTaskMain"]');
+    this.buttonBurger = this.findEle('[data-label="navtoggle"]');
+    this.labelTaskListHeading = this.findEle('[data-label="tasklistHeading"]');
+    this.tasklist = this.findEle('[data-label="tasklist"]');
+    this.buttonCloseModal = this.findEle('#close-modal');
+    this.elementModal = this.findEle('[data-label="modal"]');
+
+    // ##########[ Immediate Calls ]##########
+    this.eventToggleNav();
+    this.eventCloseModal();
+    this.eventNewTask();
+  }
   createEle(...args) {
     const [ele, ...styles] = args;
     const element = document.createElement(ele);
@@ -14,7 +28,9 @@ export default class View {
     const element = document.querySelector(selector);
     return element;
   }
-  clearTasks() {}
+  clearTasks() {
+    while (this.tasklist.firstChild) this.tasklist.firstChild.remove();
+  }
   displayTasks(tasks) {
     this.clearTasks();
   }
@@ -23,7 +39,21 @@ export default class View {
   // consider combining common event handlers into single functions
   // i.e., eventAddTask and eventAddProject both trigger on submit event
   // ##########################################
-
+  eventCloseModal() {
+    this.buttonCloseModal.addEventListener('click', (e) => {
+      this.elementModal.classList.add('hidden');
+    });
+  }
+  eventToggleNav() {
+    this.buttonBurger.addEventListener('click', (e) => {
+      this.elementNavContainer.classList.toggle('nav-hidden');
+    });
+  }
+  eventNewTask() {
+    this.buttonAddTaskMain.addEventListener('click', (e) => {
+      this.elementModal.classList.toggle('hidden');
+    });
+  }
   eventAddTask(handler) {}
   eventEditTask(handler) {}
   eventDeleteTask(handler) {}
