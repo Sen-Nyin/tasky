f6aeea3a6da1aba7bbc16fbb16fc6902f21066fa
Leon Lonsdale <leonlonsdale@icloud.com>
2022-09-27 18:09:39 +0100

    update tailwind config for additional resolution breakpoint. Updated responsive nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   content: ['./src/*.{html,js}'],
   theme: {
     extend: {},
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536',
+      '3xl': '1720px',
+    },
   },
   plugins: [],
 };
